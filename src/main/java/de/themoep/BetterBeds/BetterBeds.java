2e10faecf6c38bcd4463b9db277a9b5425ae47ea
Ferus <ferus+git@airmail.cc>
2018-05-21 17:30:34 -0700

    Support AutoAFK
    Added option to ignore AFK players as well as support for AutoAFK

--- src/main/java/de/themoep/BetterBeds/BetterBeds.java
+++ src/main/java/de/themoep/BetterBeds/BetterBeds.java
@@ -19,7 +19,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BetterBeds extends JavaPlugin implements Listener {
-	
+
 	private int minPlayers = 2;
 	private double sleepPercentage = 0.5;
 	private int nightSpeed = 0;
@@ -31,8 +31,8 @@ public class BetterBeds extends JavaPlugin implements Listener {
 	private NotificationMessage notifyMessage;
 	private NotificationMessage notifyOnSingleMessage;
 	private int transitionTask = 0;
-	private HashMap<UUID,String> nameOfLastPlayerToEnterBed = new HashMap<UUID,String>(); 
-	
+	private HashMap<UUID,String> nameOfLastPlayerToEnterBed = new HashMap<UUID,String>();
+
 	public void onEnable() {
 		this.saveDefaultConfig();
 		this.loadConfig();
@@ -49,9 +49,9 @@ public class BetterBeds extends JavaPlugin implements Listener {
 			this.loadConfig();
 			return true;
 		}
-		return false;		
+		return false;
 	}
-	
+
 	/**
 	 * Loads the options from the config file into the plugins variables
 	 */
@@ -62,11 +62,12 @@ public class BetterBeds extends JavaPlugin implements Listener {
 			this.sleepPercentage = Double.parseDouble(this.getConfig().getString("sleepPercentage").replaceAll(" ", "").replace("%", ""));
 		} catch (NumberFormatException e) {
 			this.getLogger().log(Level.WARNING,"You have an Error in your config at the sleepPercentage-node! Using the default now: " + this.sleepPercentage);
-		} 
+		}
 		if (this.sleepPercentage > 1) {
 			this.sleepPercentage = this.sleepPercentage / 100;
 		}
 		this.nightSpeed = this.getConfig().getInt("nightSpeed");
+        this.ingoreAFK = this.getConfig().getBoolean("ignoreAFK")
 		this.ghostMessage = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("msg.ghost"));
 		this.sleepMessage = new NotificationMessage(
 				NotificationType.valueOf(this.getConfig().getString("msg.sleep.type")),
@@ -89,15 +90,15 @@ public class BetterBeds extends JavaPlugin implements Listener {
 				ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("msg.notifyOnSingle.text"))
 		);
 	}
-	
+
 	/**
-	 * Calculate if number of sleeping players is enough to fast forward the night. 
+	 * Calculate if number of sleeping players is enough to fast forward the night.
 	 * @param event PlayerBedEnterEvent
 	 */
 	@EventHandler
 	public void onPlayerBedEnter(PlayerBedEnterEvent event) {
-		if(event.isCancelled() 
-				|| event.getPlayer().hasPermission("betterbeds.ignore") 
+		if(event.isCancelled()
+				|| event.getPlayer().hasPermission("betterbeds.ignore")
 				|| !event.getPlayer().hasPermission("betterbeds.sleep")
 				|| this.transitionTask != 0)
 			return;
@@ -114,17 +115,17 @@ public class BetterBeds extends JavaPlugin implements Listener {
 			if(!p.hasPermission("betterbeds.ignore"))
 				calculatedPlayers++;
 		}
-		
+
 		HashSet<UUID> playerList = new HashSet<UUID>();
-		
+
 		if(this.asleepPlayers.containsKey(world.getUID()))
 			playerList = this.asleepPlayers.get(world.getUID());
-		
+
 		playerList.add(event.getPlayer().getUniqueId());
-		
+
 		this.asleepPlayers.put(world.getUID(), playerList);
 		nameOfLastPlayerToEnterBed.put(world.getUID(), event.getPlayer().getName());
-		
+
 		this.getLogger().log(Level.INFO, event.getPlayer().getName() + " sleeps now. " + playerList.size() + "/" + calculatedPlayers + " players are asleep in world " + world.getName());
 
 		if(!this.checkPlayers(world, false))
@@ -148,12 +149,12 @@ public class BetterBeds extends JavaPlugin implements Listener {
                 if (p.hasPermission("betterbeds.sleep") && !p.hasPermission("betterbeds.ignore") && !isPlayerAFK(p))
                     calculatedPlayers++;
             }
-		}		
+		}
 		HashSet<UUID> playerList = this.asleepPlayers.get(world.getUID());
-		return (playerList.size() >= minPlayers	&& playerList.size() >= calculatedPlayers * this.sleepPercentage) 
+		return (playerList.size() >= minPlayers	&& playerList.size() >= calculatedPlayers * this.sleepPercentage)
 				|| (playerList.size() < minPlayers && playerList.size() >= calculatedPlayers);
 	}
-	
+
 	/**
 	 * Check if enough players are asleep and fast forward if so.
 	 * @param world The world to calculate with
@@ -168,7 +169,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 			} else {
 				if (transitionTask != 0)
 					return false;
-				
+
 				notifyPlayers(world, (this.asleepPlayers.get(world.getUID()).size() > 1) ? this.notifyMessage : this.notifyOnSingleMessage);
 
 				this.getLogger().log(Level.INFO, "Timelapsing " + nightSpeed + "x until dawn in world " + world.getName());
@@ -180,7 +181,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 							return;
 						}
 						long currentTime = world.getTime();
-						long newTime = currentTime + nightSpeed; 
+						long newTime = currentTime + nightSpeed;
 						if (newTime >= 23450) {
 							getServer().getScheduler().cancelTask(transitionTask);
 							transitionTask = 0;
@@ -231,7 +232,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 		world.setTime(23450);
 		if(world.hasStorm())
 			world.setStorm(false);
-		
+
 		if(world.isThundering())
 			world.setThundering(false);
 
@@ -239,7 +240,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 
 		this.asleepPlayers.get(world.getUID()).clear();
 	}
-	
+
 	/**
 	 * Recalculates the number of sleeping players if a player leaves his bed
 	 * @param event PlayerBedLeaveEvent
@@ -247,7 +248,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
 		this.calculateBedleave(event.getPlayer(), event.getBed().getWorld());
-	}	
+	}
 
 	/**
 	 * Recalculates the number of sleeping players if a player quits the game between 12500 and 100 time ticks
@@ -258,7 +259,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 		if(!this.calculateBedleave(event.getPlayer(), event.getPlayer().getWorld()))
 			this.checkPlayers(event.getPlayer().getWorld(), true);
 	}
-	
+
 	/**
 	 * Recalculates the number of sleeping players if a player changes from a normal world between 12500 and 100 time ticks
 	 * @param event PlayerChangedWorldEvent
@@ -270,7 +271,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 	}
 
 	/**
-	 * Calculates what happens when a player leaves the bed. 
+	 * Calculates what happens when a player leaves the bed.
 	 * @param player The player who left the bed
 	 * @param world The world the bed was in (because it's possible the player isn't there anymore when he existed it)
 	 * @return boolean - True if we don't need to check the players anymore, False if didn't get checked if we should fast forward
@@ -281,14 +282,14 @@ public class BetterBeds extends JavaPlugin implements Listener {
 			return true;
 		if(!this.asleepPlayers.containsKey(world.getUID()))
 			return true;
-		
+
 		if(this.asleepPlayers.get(world.getUID()).contains(player.getUniqueId())) {
 			int calculatedPlayers = countQualifyingPlayers(world);
 
 			this.asleepPlayers.get(world.getUID()).remove(player.getUniqueId());
-	
+
 			HashSet<UUID> playerList = this.asleepPlayers.get(world.getUID());
-	
+
 			this.getLogger().log(Level.INFO, player.getName() + " is not sleeping anymore. " + playerList.size() + "/" + calculatedPlayers + " players are asleep in world " + world.getName());
 
 			notifyPlayers(world, this.leaveMessage);
@@ -306,17 +307,23 @@ public class BetterBeds extends JavaPlugin implements Listener {
 	 */
 	private int countQualifyingPlayers(World world) {
 		int calculatedPlayers = 0;
-		for(Player p : getServer().getOnlinePlayers())
-			if(world.equals(p.getWorld()) && !p.hasPermission("betterbeds.ignore") && p.hasPermission("betterbeds.sleep") && !isPlayerAFK(p))
-				calculatedPlayers ++;
-		return calculatedPlayers;
+        // This may be a little dirty..
+        if (this.ingoreAFK == 0) {
+            for(Player p : getServer().getOnlinePlayers())
+                if(world.equals(p.getWorld()) && !p.hasPermission("betterbeds.ignore") && p.hasPermission("betterbeds.sleep") && !isPlayerAFK(p))
+                calculatedPlayers ++;
+        } else {
+            for(Player p : getServer().getOnlinePlayers())
+                if(world.equals(p.getWorld()) && !p.hasPermission("betterbeds.ignore") && p.hasPermission("betterbeds.sleep") && isPlayerAFK(p))
+                calculatedPlayers ++;
+        };
+        return calculatedPlayers;
 	}
 
 	/**
 	 * Check if a player is AFK
 	 * Currently this only works with the WhosAFK plugin
 	 * TODO: Add support for checking with more methods
-	 * TODO: Add a config option to decide whether AFK players should be counted
 	 * @param Player
 	 * @return boolean - True if Player is currently AFK
 	 */
@@ -338,6 +345,22 @@ public class BetterBeds extends JavaPlugin implements Listener {
 				return true;
 		} catch (ClassNotFoundException e) {
 			// WhosAFK is not installed, no need to panic
+            // Perhaps AutoAFK is installed too..
+            try {
+                // Load the AutoAFK class and it's playerIsAFK(Player) method
+                Class<?> AutoAFK = classLoader.loadClass("autoafk.Main");
+                Method autoafkPlayerIsAFK = AutoAFK.getMethod("playerIsAFK", Player.class);
+
+                // Get the instance of AutoAFK being used
+                @SuppressWarnings({ "unchecked", "rawtypes" })
+                JavPlugin autoafk = JavaPlugin.getPlugin((Class) AutoAFK);
+
+                // Check if AutoAFK thinks the player is AFK
+                if (autoafk.isEnabled() && (Boolean) autoafkPlayerIsAFK.invoke(autoafk, p))
+                    return true;
+            } catch (ClassNotFoundException e) {
+                // AutoAFK is not installed
+            }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -349,7 +372,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 	/**
 	 * Converts eventual parameters in a message into its real values.
 	 * TODO: Make it so that not every parameter is required!
-	 * @param msg String of the message to convert 
+	 * @param msg String of the message to convert
 	 * @param playername String of the playername to insert in the message
 	 * @param sleeping Integer of sleeping players
 	 * @param online Integer of online players in world
@@ -366,7 +389,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
 		msg = msg.replace("{more}", Integer.toString(more));
 		return msg;
 	}
-    
+
     private List<Player> getPlayers(World world) {
         List<Player> pList = new ArrayList<Player>();
         for(Player p : getServer().getOnlinePlayers()) {
